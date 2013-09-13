891fd8e726572e573b408211cac3e9f6632f70a8
dbc-apprentice <dbc@dbc.com>
2013-09-12 19:41:29 -0700

    ALL DONE

---
+++ spec/legislator_spec.rb
@@ -0,0 +1,19 @@
+# # require 'rspec'
+
+require_relative '../app/models/legislator'
+#  puts "Reps from CA"
+# Legislator.get_reps_from('CA')
+
+# puts
+# puts "Sens from CA"
+# Legislator.get_senators_from('CA')
+
+# puts
+# puts "Reps who are R"
+# Legislator.get_reps_who_are('R')
+
+# puts
+# puts "Senators who are D"
+# Legislator.get_senators_who_are('D')
+
+p Legislator.a
